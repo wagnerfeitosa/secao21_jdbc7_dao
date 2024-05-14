111a98f1f14c8538463fd56d1cfed88cf97b0cbd
wagner marques <wagenrfeitosa2000@hotmail.com>
2024-05-14 14:17:22 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/Impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.Impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
