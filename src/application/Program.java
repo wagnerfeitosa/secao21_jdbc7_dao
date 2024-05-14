51e719b77861a9c3bfb97a4ee225b6135eae44e6
wagner marques <wagenrfeitosa2000@hotmail.com>
2024-05-14 09:55:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		/*TESTE DOS OBJETOS*/
+		Department obj = new Department(1,"Books");
+		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,obj);
 		
-		
+		System.out.println(seller);
 
 	}
 
