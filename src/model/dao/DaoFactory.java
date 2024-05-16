68497bf3b9a73fdf82df73de705294ccbca04744
wagner marques <wagenrfeitosa2000@hotmail.com>
2024-05-16 07:38:09 -0300

    findyById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
+	//CLASSE RESPONSAVEL PARA INSTACIAR SELLERDAO() RECEBENDO DB.CONNECTION
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
